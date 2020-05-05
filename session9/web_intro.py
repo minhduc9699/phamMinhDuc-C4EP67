43bbb6eb86006ef2dd828de7e105e2f3ea42537b
Phạm Đức <minhduc.096.99@gmail.com>
2020-05-05 19:32:52 +0700

    .

--- session9/web_intro.py
+++ session9/web_intro.py
@@ -1,5 +1,6 @@
 from flask import *
 from pymongo import MongoClient
+from bson import ObjectId
 
 client = MongoClient('localhost:27017')
 db = client['c4e']
@@ -12,8 +13,20 @@ def homepage():
   idols = kpop_collection.find({})
   return render_template('homepage.html', idols_data=idols)
 
-@app.route('/me')
-def my_info():
-  return 'this is me'
 
+@app.route('/idol/<id>')
+def detail(id):
+  idol = kpop_collection.find_one({'_id': ObjectId(id)})
+  return render_template('detail.html', idol=idol)
+
+
+@app.route('/delete/<id>')
+def delete(id):
+  kpop_collection.delete_one({'_id': ObjectId(id)})
+  return redirect('/')
+
+@app.route('/goback')
+def goback():
+
+  return redirect('/')
 app.run(debug=True, port=3000)
